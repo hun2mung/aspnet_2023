2844998067c112ab6257873e6636a297f7f2f072
hun2mung <ldh5444@naver.com>
2023-05-26 12:38:18 +0900

    23-05-26

---
+++ Day08/Day08_Web/aspnet03_portpoliowebapp/Controllers/AccountController.cs
@@ -0,0 +1,154 @@
+﻿using aspnet02_boardapp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aspnet02_boardapp.Controllers
+{
+    // 사용자 회원가입, 로그인, 로그아웃
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            // 생성자 마법사. Null 검사 추가
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+        // https://localhost:7059/Account/Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // public IActionResult Register(RegisterModel model)
+        // 비동기가 아니면 return 값은 IActionResult(위)
+        // 비동기면 Task<IActionResult>(아래)
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            ModelState.Remove("PhoneNumber");   // PhoneNumber는 입력값 검증에서 제거
+            if (ModelState.IsValid) // 데이터 입력 후 검증 성공 시
+            {
+                var user = new IdentityUser()
+                {
+                    // ASP.NET user - aspnetusers 테이블에 데이터넣기 위해서
+                    // 매핑되는 인스턴스를 생성
+                    UserName=model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber // 핸드폰 번호 추가
+                };
+
+                // aspnetusers 테이블에 사용자 데이터를 대입
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    // 회원가입을 성공했으면 로그인한 뒤 localhost:7059/Home/Index로!
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    // 토스트 메시지
+                    TempData["succeed"] = "회원가입 되었습니다."; // 성공 메세지
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model); // 회원가입을 실패하면 그 화면 그대로 유지
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                // 파라미터 순서 : 아이디, 패스워드, isPersistent = RememberMe, 실패할 때 계정 잠그기
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if(result.Succeeded)
+                {
+                    // 토스트 메시지
+                    TempData["succeed"] = "로그인 되었습니다."; // 성공 메세지
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "로그인 실패!");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            // 토스트 메시지
+            TempData["succeed"] = "로그아웃 되었습니다."; // 성공 메세지
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Profile(string userName)
+        {
+            Debug.WriteLine(userName);
+
+            var curUser = await _userManager.FindByNameAsync(userName);
+
+            if(curUser == null)
+            {
+                TempData["error"] = "사용자가 없습니다.";
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new RegisterModel()
+            {
+                Email = curUser.Email,
+                PhoneNumber = curUser.PhoneNumber
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                
+                user.PhoneNumber = model.PhoneNumber;
+                user.Email = model.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, model.Password); // 비밀번호 변경!
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    TempData["succeed"] = "프로필 변경되었습니다."; // 성공 메세지
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model); // 프로필 변경 실패 시 화면 유지.
+        }
+
+    }
+}
---
+++ Day08/Day08_Web/aspnet03_portpoliowebapp/Models/CreateRoleModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet02_boardapp.Models
+{
+    public class CreateRoleModel
+    {
+        [Required(ErrorMessage ="권한명은 필수!")]
+        [DisplayName("권한 명")]
+        public string RoleName { get; set; }    // Admin, User
+    }
+}
